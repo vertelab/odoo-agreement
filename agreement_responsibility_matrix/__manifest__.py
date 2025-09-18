3df186f64a7bbc727431e10a592a984dd8407400
Jakob Krabbe <jakob.krabbe@vertel.se>
2025-09-18 12:35:38 +0000

    inserting demo data

--- agreement_responsibility_matrix/__manifest__.py
+++ agreement_responsibility_matrix/__manifest__.py
@@ -4,16 +4,22 @@
 {
     "name": "Agreement Responsibility Matrix",
     "summary": "Adds an agreement responsibility matrix",
-    "version": "18.0.1.0.0",
+    "version": "1.0.0",
     "category": "Contract",
     "author": "Vertel AB",
     "website": "https://github.com/vertelab/odoo-agreement",
     "license": "AGPL-3",
-    "depends": ["agreement"],
+    "depends": ["agreement","agreement_legal"],
+
     "data": [
         "security/ir.model.access.csv",
         "views/agreement_view.xml",
         "views/agreement_responsibility_category_view.xml",
+        'demo/agreement_demo.xml',
+    ],
+    'demo': [
+        #'demo/agreement_demo.xml',
     ],
     "installable": True,
+    'application': False,
 }
--- agreement_responsibility_matrix/models/agreement.py
+++ agreement_responsibility_matrix/models/agreement.py
@@ -7,3 +7,19 @@ class Agreement(models.Model):
     agreement_responsibility_line_ids = fields.One2many(
         "agreement.responsibility.line", "agreement_id", "Agreement Responsibility Lines"
     )
+    category_id = fields.Many2one('agreement.category', string='Category')
+    parent_id = fields.Many2one('agreement', string='Parent Agreement', ondelete='cascade')
+    child_ids = fields.One2many('agreement', 'parent_id', string='Child Agreements')
+
+    
+class AgreementCategory(models.Model):
+    _name = 'agreement.category'
+    _description = 'Agreement Category'
+    _parent_name = 'parent_id'
+    _parent_store = True
+
+    name = fields.Char(required=True)
+    parent_id = fields.Many2one('agreement.category', string='Parent Category', index=True, ondelete='cascade')
+    child_ids = fields.One2many('agreement.category', 'parent_id', string='Children')
+    
+    parent_path = fields.Char(index=True)
