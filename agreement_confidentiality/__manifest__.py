2c3ba023b2ec5bfb5422ca7d05f6ab476258dd5e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2026-01-12 10:59:10 +0100

    T/7043 Update __manifest__.py

--- agreement_confidentiality/__manifest__.py
+++ agreement_confidentiality/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Agreement Confidentiality',
+    'name': 'Agreement: Confidentiality',
     'version': '1.2',
     'category': 'Agreement',
     'depends': ['agreement_legal'],
