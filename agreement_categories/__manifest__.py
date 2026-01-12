aa722d5c5e8ec3afbcc160a0d84846c331e5ba8b
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2026-01-12 10:58:44 +0100

    T/7043 Update __manifest__.py

--- agreement_categories/__manifest__.py
+++ agreement_categories/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Agreement Categories',
+    'name': 'Agreement: Categories',
     'version': '1.2',
     'category': 'Agreement',
     'depends': ['agreement_legal', 'mail'],
