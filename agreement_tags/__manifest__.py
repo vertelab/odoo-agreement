9c51db6387a09363916ce559d614346d1917b01d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2026-01-12 11:00:08 +0100

    T/7043 Update __manifest__.py

--- agreement_tags/__manifest__.py
+++ agreement_tags/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Agreement Tags',
+    'name': 'Agreement: Tags',
     'version': '1.2',
     'category': 'Agreement',
     'depends': ['agreement_legal', 'mail'],
